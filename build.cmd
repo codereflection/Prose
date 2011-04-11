df6056851ae3291447a7ba6ffcd82099ed514429
Jeff Schumacher <jeff@codingreflection.com>
2011-04-11 00:26:28 -0700

    Setting up WPF application with build command

---
+++ build.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+IF NOT EXIST out mkdir out
+
+csc /target:winexe /out:.\out\prose.exe /r:System.dll /r:c:\windows\microsoft.net\Framework\v4.0.30319\WPF\WindowsBase.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationCore.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationFramework.dll .\src\Prose\Prose.cs
---
+++ src/Prose/Prose.cs
@@ -0,0 +1,15 @@
+// Prose.cs
+using System;
+using System.Windows;
+
+namespace Prose
+{
+    class ProseApplication
+    {
+        [STAThread]
+        static void Main()
+        {
+            MessageBox.Show("Prose - because code should be beautiful.");
+        }
+    }
+}
