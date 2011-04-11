b45ca500d7a52691ef6f31522bb64b32539462ff
Jeff Schumacher <jeff@codingreflection.com>
2011-04-11 00:29:05 -0700

    Adding recurse to build command

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@
 
 IF NOT EXIST out mkdir out
 
-csc /target:winexe /out:.\out\prose.exe /r:System.dll /r:c:\windows\microsoft.net\Framework\v4.0.30319\WPF\WindowsBase.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationCore.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationFramework.dll .\src\Prose\Prose.cs
+csc /target:winexe /out:.\out\prose.exe /r:System.dll /r:c:\windows\microsoft.net\Framework\v4.0.30319\WPF\WindowsBase.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationCore.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationFramework.dll /recurse:src\*.cs /nologo
