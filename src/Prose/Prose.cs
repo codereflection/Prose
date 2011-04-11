122389c1a77f08953e4480729c3ba23516437caf
Jeff Schumacher <jeff@codingreflection.com>
2011-04-11 01:10:58 -0700

    Setting up textbox for editing

--- src/Prose/Prose.cs
+++ src/Prose/Prose.cs
@@ -1,6 +1,7 @@
 // Prose.cs
 using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Prose
 {
@@ -16,11 +17,28 @@ namespace Prose
 
     public class ProseApplication : Application
     {
+        public ProseApplication()
+        {
+        }
+
+        void BuildControls(Window window)
+        {
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            var textbox = new TextBox { AcceptsReturn = true, AcceptsTab = true };
+            Grid.SetColumn(textbox, 0);
+            Grid.SetRow(textbox, 0);
+            grid.Children.Add(textbox);
+            window.Content = grid;
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
              base.OnStartup(e);
 
              var window = new Window();
+             BuildControls(window);
              window.Title = "Prose - because code should be beautiful...";
              window.Show();
         }
