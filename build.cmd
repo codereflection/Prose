eb4cb9a2167622aeac6fd73a64f086c53ab563a4
Jeff Schumacher <jeff@codingreflection.com>
2011-04-11 00:52:57 -0700

    Turning Prose into a proper window

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@
 
 IF NOT EXIST out mkdir out
 
-csc /target:winexe /out:.\out\prose.exe /r:System.dll /r:c:\windows\microsoft.net\Framework\v4.0.30319\WPF\WindowsBase.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationCore.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationFramework.dll /recurse:src\*.cs /nologo
+csc /target:winexe /out:.\out\prose.exe /r:System.dll /r:c:\windows\microsoft.net\Framework\v4.0.30319\WPF\WindowsBase.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationCore.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\WPF\PresentationFramework.dll /r:C:\windows\microsoft.net\Framework\v4.0.30319\System.Xaml.dll /recurse:src\*.cs /nologo
--- src/Prose/Prose.cs
+++ src/Prose/Prose.cs
@@ -4,12 +4,25 @@ using System.Windows;
 
 namespace Prose
 {
-    class ProseApplication
+    public class EntryPoint
     {
         [STAThread]
-        static void Main()
+        public static void Main()
         {
-            MessageBox.Show("Prose - because code should be beautiful.");
+            var app = new ProseApplication();
+            app.Run();
+        }
+    }
+
+    public class ProseApplication : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+             base.OnStartup(e);
+
+             var window = new Window();
+             //window.Text = "Prose - because code should be beautiful...";
+             window.Show();
         }
     }
 }
